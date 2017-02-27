e129bc4f2dd52300304c400b5e9fbb257da6c30a
Benga AKingbe <g.akingbe@gmail.com>
2017-02-26 22:23:33 -0500

    Final Commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,20 @@
-var words = ["Wizards", "Raptors", "Warriors", "Celtics", "Pacers", "Cavaliers", "Lakers", "Pelicans", "Clippers", "Mavericks"]
+var words = ["wizards", "raptors", "warriors", "celtics", "pacers", "cavaliers", "lakers", "pelicans", "clippers", "mavericks"]
 
-var chosenWord = "";
-var lettersInChosenWord = [];
+var currentWord = "";
+var lettersInCurrentWord = [];
 var numBlanks = 0;
 var blanksAndSuccesses = [];
 var wrongGuesses = [];
 
 var winCounter = 0;
-var lossCounter = 0;
+var lossCounter = 1;
 var numGuesses = 12;
+
 
 
 function startGame() {
 /*
-1. select a word at random (Math.random)
+1. choose a random word (Math.random)
 2. break up random word into letters and replace them with underscores
 3. add those underscores to the HTML
 4. numGuesses always equals 12 and blankAndSuccesses is an empty array, and wrongGuesses is also empty
@@ -23,16 +24,17 @@ numGuesses = 12;
 blanksAndSuccesses = [];
 wrongGuesses = [];
 
-chosenWord = words[Math.floor(Math.random() * words.length)];
-lettersInChosenWord = chosenWord.split("");
-numBlanks = lettersInChosenWord.length;
-console.log(chosenWord)
-console.log(numBlanks)
+currentWord = words[Math.floor(Math.random() * words.length)];
+lettersInCurrentWord = currentWord.split("");
+numBlanks = lettersInCurrentWord.length;
+
 
 for (var i = 0; i < numBlanks; i++) {
-	blanksAndSuccesses.push("_")
+	blanksAndSuccesses.push("_");
 }
-console.log(blanksAndSuccesses)
+
+document.getElementById("remaining").innerHTML = numGuesses;
+document.getElementById("blank-word").innerHTML = blanksAndSuccesses.join(" ");
 
 
 }
@@ -40,12 +42,31 @@ console.log(blanksAndSuccesses)
 function checkLetters(letter) {
 
 /*
-1. Compare the letter the user picks matches any of the letters in the word
+1. Compare the letter the user picks to see if it matches any of the letters in the word
 2. conditional statement to determine if the letter the user picks is in the word. If so, do something. If not, do something else
 3. if the user is wrong, we want to decrease the numGuesses variable by onw
-
 */	
 
+var letterInWord = false;
+
+for (var i = 0; i < numBlanks; i++) {
+	if (currentWord[i] === letter) {
+		letterInWord = true;
+	}
+}
+
+	if(letterInWord) {
+		for (i = 0; i < numBlanks; i++) {
+			if (currentWord[i] === letter) {
+				blanksAndSuccesses[i] = letter;
+			}
+			
+		}
+	} else {
+		numGuesses --;
+		wrongGuesses.push(letter)
+	}
+	
 }
 
 function roundComplete() {
@@ -58,17 +79,44 @@ function roundComplete() {
 
 */
 
+document.getElementById("blank-word").innerHTML = blanksAndSuccesses.join(" ");
+document.getElementById("remaining").innerHTML = numGuesses;
+document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
+
+
+
+
+if (lettersInCurrentWord.join(" ") === blanksAndSuccesses.join("")) {
+	winCounter++;
+	alert("You win!");
+	document.getElementById("win-counter").innerHTML = winCounter;
+	startGame();
+}
+else if (numGuesses === 0) {
+	document.getElementById("loss-counter").innerHTML = lossCounter++;
+	document.getElementById("wrong-guesses").innerHTML = "";
+	alert("You lose...");
+	wrongGuesses = [];
+	startGame();
+}
 
 }
 
 
+
+
+startGame();
+
 document.onkeyup = function(event) {
 /*
 1. take in the letter that we type in
+
 2. pass the letter through the checkLetter function
 
 */
-
+var letterGuess = String.fromCharCode(event.keyCode).toLowerCase();
+checkLetters(letterGuess)
+roundComplete();
 
 }
 
